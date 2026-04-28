8041d35e35a9f3ef9651cbf16fc69ca44f690096
lbarco248-glitch <lbarco248@gmail.com>
2026-04-27 22:55:45 -0500

    Modified dial method in CellPhone and completed CellPhone Service exercise.

--- src/main/java/WorkbookPractice/CellPhoneApplication.java
+++ src/main/java/WorkbookPractice/CellPhoneApplication.java
@@ -2,29 +2,59 @@ package WorkbookPractice;
 import java.util.Scanner;
 
 public class CellPhoneApplication {
-    public static void main(String[] args)
-    {
-        CellPhone Phone = new CellPhone();
-        Scanner input = new Scanner(System.in);
-        System.out.print("What is the serial number? ");
-        Phone.setSerialNumber(input.nextInt());
-        System.out.println(Phone.getSerialNumber());
-        System.out.print("What model is the phone? ");
-        input.nextLine();
-        Phone.setModel(input.nextLine());
-        System.out.println(Phone.getModel());
-        System.out.print("Who is the carrier? ");
-        Phone.setCarrier(input.nextLine());
-        System.out.println(Phone.getCarrier());
-        System.out.print("What is the phone number? ");
-        Phone.setPhoneNumber(input.nextLine());
-        System.out.println(Phone.getPhoneNumber());
-        System.out.print("Who is the owner of the phone? ");
-        Phone.setOwner(input.nextLine());
-        System.out.println(Phone.getOwner());
-        input.close();
+    public static void main(String[] args) {
 
-        String test = Phone.dial(Phone.getOwner(), Phone.getPhoneNumber());
-        System.out.println(test);
+            CellPhone Phone1 = new CellPhone();
+            // Object 1 of CellPhone
+            Scanner input = new Scanner(System.in);
+            System.out.print("What is the serial number? ");
+            Phone1.setSerialNumber(input.nextInt());
+            System.out.println(Phone1.getSerialNumber());
+            System.out.print("What model is the phone? ");
+            input.nextLine();
+            Phone1.setModel(input.nextLine());
+            System.out.println(Phone1.getModel());
+            System.out.print("Who is the carrier? ");
+            Phone1.setCarrier(input.nextLine());
+            System.out.println(Phone1.getCarrier());
+            System.out.print("What is the phone number? ");
+            Phone1.setPhoneNumber(input.nextLine());
+            System.out.println(Phone1.getPhoneNumber());
+            System.out.print("Who is the owner of the phone? ");
+            Phone1.setOwner(input.nextLine());
+            System.out.println(Phone1.getOwner());
+
+            String test = Phone1.getOwner() + "'s" + Phone1.dial(Phone1.getPhoneNumber());
+            // Used concatenation to avoid issues for future use of dial method
+            System.out.println(test);
+
+            CellPhone Phone2 = new CellPhone();
+            //Object 2 of CellPhone
+            System.out.print("What is the serial number? ");
+            Phone2.setSerialNumber(input.nextInt());
+            System.out.println(Phone2.getSerialNumber());
+            System.out.print("What model is the phone? ");
+            input.nextLine();
+            Phone2.setModel(input.nextLine());
+            System.out.println(Phone2.getModel());
+            System.out.print("Who is the carrier? ");
+            Phone2.setCarrier(input.nextLine());
+            System.out.println(Phone2.getCarrier());
+            System.out.print("What is the phone number? ");
+            Phone2.setPhoneNumber(input.nextLine());
+            System.out.println(Phone2.getPhoneNumber());
+            System.out.print("Who is the owner of the phone? ");
+            Phone2.setOwner(input.nextLine());
+            System.out.println(Phone2.getOwner());
+            input.close();
+            display(Phone1);
+            display(Phone2);
+        Phone1.dial(Phone2.getPhoneNumber());
+        //Phone 1 is dialing Phone 2 using the dial method in CellPhone
+        Phone2.dial(Phone1.getPhoneNumber());
+    }
+    public static void display(CellPhone phone)
+    {
+        System.out.printf("Serial Number: %d", phone.getSerialNumber());
     }
 }
